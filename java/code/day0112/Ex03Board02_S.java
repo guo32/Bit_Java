5749c0a8c1c1ed0f1fc1414c4dad9371349cba75
guo32 <63935531+guo32@users.noreply.github.com>
2023-01-14 08:31:39 +0900

    Update Ex03Board02_S.java

--- java/code/day0112/Ex03Board02_S.java
+++ java/code/day0112/Ex03Board02_S.java
@@ -3,6 +3,7 @@ package day0112;
 import util.ScannerUtil;
 import viewer.Board2Viewer;
 import viewer.CommentViewer;
+import viewer.ReplyViewer;
 import viewer.UserViewer;
 
 import java.util.Scanner;
@@ -14,9 +15,13 @@ public class Ex03Board02_S {
         Board2Viewer board2Viewer = new Board2Viewer(scanner);
         UserViewer userViewer = new UserViewer(scanner);
         CommentViewer commentViewer = new CommentViewer(scanner);
+        ReplyViewer replyViewer = new ReplyViewer(scanner);
 
         userViewer.setBoard2Viewer(board2Viewer);
+        userViewer.setReplyViewer(replyViewer);
+
         board2Viewer.setUserViewer(userViewer, commentViewer);
+        board2Viewer.setReplyViewer(replyViewer);
         commentViewer.setBoard2Viewer(board2Viewer);
 
         userViewer.showIndex();
